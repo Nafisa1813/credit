b706b5c6cc387caac6dc33754874c91f47cf60f9
Nafisa1813 <51263005+Nafisa1813@users.noreply.github.com>
2019-08-25 14:43:03 +0530

    Add files via upload

---
+++ trial1.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 13:06:59 2019
+
+@author: Harshi
+"""
+
+# import the external libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the dataset from the csv file using pandas
+data = pd.read_csv('creditcard.csv')
+
+data.head()
+# Only use the 'Amount' and 'V1', ..., 'V28' features
+features = ['V%d' % number for number in range(1, 29)] + ['Amount']
+#In above, I used '4' to limit the subplots but actually we have to use '29'.
+
+# The target variable which we would like to predict, is the 'Class' variable
+target = 'Class'
+
+# Now create an X variable (containing the features) and an y variable (containing only the target variable)
+X = data[features]
+y = data[target]
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+scaler.fit(X)
+
+# Plot histograms of each parameter 
+X.hist(figsize = (20, 20))
+plt.show()
+#Split the data set using 'train_test_split' function
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+
+# Instantiate the model to an empty object
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+
+# Train the model using 'fit' method
+model.fit(X_train, y_train)
+
+# Test the model using 'predict' method
+y_pred = model.predict(X_test)
+
+# Print the classification report 
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
